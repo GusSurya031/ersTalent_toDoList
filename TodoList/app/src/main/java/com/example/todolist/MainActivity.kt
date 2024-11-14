6e233f02e3a5ae9be24f22ece7cd56ae229dde10
Slush03 <taufiq.ttgi@gmail.com>
2024-11-14 11:14:35 +0700

    Homework

---
+++ TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.todolist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todolist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), TaskItemClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        binding.newTaskButton.setOnClickListener {
+            NewTasksheet(null).show(supportFragmentManager, "newTaskTag")
+        }
+        setRecyclerView()
+    }
+    private fun setRecyclerView(){
+        val mainActivity = this
+        taskViewModel.taskItems.observe(this){
+            binding.todoListReclerView.apply {
+                layoutManager = LinearLayoutManager(applicationContext)
+                adapter = TaskItemAdapter(it, mainActivity)
+            }
+        }
+    }
+
+    override fun editTaskItem(taskItem: TaskItem) {
+        NewTasksheet(taskItem).show(supportFragmentManager, "newTaskTag")
+    }
+
+    override fun completeTaskItem(taskItem: TaskItem) {
+        taskViewModel.setCompleted(taskItem)
+    }
+}
